0c4d9ef13612b5c05894156a6840cd6284e49d81
lucavenir <luca@venir.dev>
2022-01-25 18:07:54 +0100

    Implemented a first index page

--- openmed-svelte/src/routes/index.svelte
+++ openmed-svelte/src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export async function load({ session }) {
+    // If the user's not logged in, re-route him to the login page
+    if(!session?.id) {
+      return {
+        status: 302,
+        redirect: "/login"
+      };
+    }
+
+    // TODO: Implement a home / dashboard page
+    // const user = await fetch('http://localhost:3001/v1/users/currentuser');
+
+    return {
+      props: {
+        ...session
+      }
+    };
+  };
+</script>
+
+<script>
+  export let email;
+  export let id;
+</script>
+
+<svelte:head>
+  <title>OpenMed</title>
+</svelte:head>
+
+
+<!-- This is just a trial, PLEASE DO NOT TAKE THIS AS SERIOUS CODE -->
+<h1 class='text-2xl font-semibold text-center'>
+  Ciao {email}!
+</h1>
+<p>
+  Questo è il tuo ID {id}!
+</p>
+
+<style>
+
+</style>
