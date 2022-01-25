c88361092b8f578ef041de89c6a9b3de0e649cfa
lucavenir <luca@venir.dev>
2022-01-25 18:05:27 +0100

    Added a global error page

---
+++ openmed-svelte/src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<!-- TODO Implement a proper error page -->
+
+<script context="module">
+  export async function load({ error, status }) {
+    return {
+      status: status,
+      props: {
+        err: `${status}: ${error.message}`
+      }
+    };
+  }
+</script>
+
+
+<script>
+  export let err;
+</script>
+
+
+<h1>Something went wrong</h1>
+<p>{err}</p>
+
+
+
+<style>
+
+</style>
