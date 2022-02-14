5df0873e5d4bc8a549614614ffb2f551b8f4c453
lucavenir <luca@venir.dev>
2022-02-14 19:42:25 +0100

    Added a simple appointment confirmation page

---
+++ openmed-svelte/src/routes/appointment/ok.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { goto } from "$app/navigation";
+</script>
+
+<h1>Appuntamento registrato correttamente</h1>
+<sub>Continua la navigazione</sub>
+
+<button on:click={() => goto('/')}>Torna in Home</button>
+
+<div on:click={() => goto('/appointment')}>Crea Nuovo Appuntamento</div>
