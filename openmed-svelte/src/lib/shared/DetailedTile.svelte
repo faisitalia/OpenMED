65de0f9cb0b9a96045cd4865a96016b21bb8e4fd
lucavenir <luca@venir.dev>
2022-02-15 11:54:52 +0100

    Created a generic-purpose detail view Tile

---
+++ openmed-svelte/src/lib/shared/DetailedTile.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let title;
+  export let subtitle;
+  export let iconImgPath;
+  export let iconImgAlt = "icon";
+
+  let isExpanded = false;
+  function toggle() {
+    isExpanded = !isExpanded;
+  }
+</script>
+
+<div id="container" on:click="{() => toggle()}">
+  <img src="{iconImgPath}" alt="{iconImgAlt}">
+  <h2>{title}</h2>
+  {#if !isExpanded}
+    <p>{subtitle}</p>
+  {:else}
+    <slot>
+      <p>{subtitle}</p>
+    </slot>
+  {/if}
+  <!-- TODO add reactive arrow -->
+</div>
