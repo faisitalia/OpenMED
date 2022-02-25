f87027d2ca4c4e63c369033b80b268cd7c7f0db2
lucavenir <luca@venir.dev>
2022-02-25 18:41:33 +0100

    Styled the appointment confirmation page

--- openmed-svelte/src/routes/appointments/ok.svelte
+++ openmed-svelte/src/routes/appointments/ok.svelte
@@ -1,14 +1,27 @@
 <script>
-  import { goto } from "$app/navigation";
+  import { goto } from '$app/navigation';
 </script>
 
 <svelte:head>
   <title>Tutto fatto! - OpenMed</title>
 </svelte:head>
 
-<h1>Appuntamento registrato correttamente</h1>
-<p>Continua la navigazione</p>
+<div
+  class="flex flex-col justify-start items-center rounded-3xl bg-brandBlue-50/50 p-10 mx-20 my-36"
+>
+  <h1 class="font-bold text-center">Appuntamento registrato correttamente</h1>
+  <p class="my-2">Continua la navigazione</p>
+  <div class="my-7" />
+  <a href="/" class="bg-brandBlue-500 text-white px-3 py-2 rounded-3xl font-bold">
+    Torna in Home
+  </a>
+  <!-- <div on:click={() => goto('/appointments/new')}>Crea Nuovo Appuntamento</div> -->
 
-<button on:click={() => goto('/')}>Torna in Home</button>
-
-<div on:click={() => goto('/appointments/new')}>Crea Nuovo Appuntamento</div>
+  <!-- Aggiungere il + in fondo a dx -->
+</div>
+<a
+  href="/appointments/new"
+  class="fixed bottom-7 right-7 material-icons p-1 rounded-full bg-brandBlue-500 font-bold text-white"
+>
+  add
+</a>
