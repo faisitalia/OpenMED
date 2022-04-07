179bd77a6d22512d9c6f2fcb522a03aea4680541
Luca Venir <luca@venir.dev>
2022-04-07 16:38:30 +0200

    Improved the style of the confirmation message in the `ok` page

--- openmed-svelte/src/routes/appointments/ok.svelte
+++ openmed-svelte/src/routes/appointments/ok.svelte
@@ -2,7 +2,9 @@
   <title>Tutto fatto! - OpenMed</title>
 </svelte:head>
 
-<div class="flex flex-col justify-center items-center rounded-3xl bg-brandBlue-50/40 p-10 my-36">
+<div
+  class="flex flex-col justify-center items-center rounded-3xl bg-brandBlue-50/40 mx-4 p-10 my-36"
+>
   <h1 class="font-bold text-center">Appuntamento registrato correttamente</h1>
   <p class="my-2 text-center">Continua la navigazione</p>
   <div class="my-7" />
@@ -13,7 +15,7 @@
 
   <!-- Aggiungere il + in fondo a dx -->
 </div>
-<div class="fixed bottom-20 right-0">
+<div class="fixed -bottom-20 right-0 mx-4">
   <div class="my-3 group flex flex-row justify-end items-center">
     <p
       class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
