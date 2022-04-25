fa261a463fc6f7cf9dba44797cb367704acf6c49
Luca Venir <luca@venir.dev>
2022-04-25 11:36:14 +0200

    Changed the API of the styled button

--- openmed-svelte/src/lib/shared/StyledButton.svelte
+++ openmed-svelte/src/lib/shared/StyledButton.svelte
@@ -1,8 +1,8 @@
 <script>
   import { createEventDispatcher } from 'svelte';
 
-  export { classes as class };
-  let classes = 'rounded-3xl';
+  export { additionalClasses as class };
+  let additionalClasses = '';
 
   export let colors = 'text-white bg-brandBlue-500 hover:bg-brandBlue-700';
   export let spaces = 'px-4 py-2 my-1';
@@ -11,7 +11,7 @@
 </script>
 
 <button
-  class="{classes} {colors} {spaces}"
+  class="rounded-3xl {additionalClasses} {colors} {spaces}"
   on:click|stopPropagation|preventDefault={() => dispatch('click')}
 >
   <slot />
