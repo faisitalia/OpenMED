b310769555b6b8d3bab94faf53c533b6927baa1f
mj8284 <lecce.ma@gmail.com>
2022-05-07 15:06:53 +0200

    added the role creation endpoint

---
+++ api/src/routes/auth/__test__/create-role.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest'
+import { constants } from 'http2'
+
+import { app } from '../../../app'
+import { deleteRoleByRoleName } from '../../../services/auth'
+
+it('returns a 200 on successful role creation', async () => {
+  const roleName = 'myRole'
+  const { body: role } = await request(app)
+    .post('/v1/roles')
+    .send({
+      name: roleName,
+    })
+    .expect(constants.HTTP_STATUS_CREATED)
+
+  expect(role).toBeDefined()
+
+  console.log(role)
+  await deleteRoleByRoleName(roleName)
+})
