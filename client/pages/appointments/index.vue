084e4f54c49e394ab77ac2fc9c648bc0f3d2cdcd
venir.dev <luca@venir.dev>
2022-06-18 11:12:06 +0200

    Migrated the appointments page

--- client/pages/appointments/index.vue
+++ client/pages/appointments/index.vue
@@ -1,160 +1,160 @@
-<script context="module">
-  import { visitsEndpoint } from '$lib/uri.js';
+<script setup>
+// import { visitsEndpoint } from '$lib/uri.js';
+const props = defineProps({
+  visits: [],
+  // userId: String,
+});
+useRouter();
+useHead({
+  title: `I tuoi appuntamenti`,
+});
 
-  export async function load({ session, fetch }) {
-    // if(!session?.id) {
-    //   return {
-    //     status: 302,
-    //     redirect: "/login"
-    //   };
-    // }
+async function load({ session, fetch }) {
+  // if(!session?.id) {
+  //   return {
+  //     status: 302,
+  //     redirect: "/login"
+  //   };
+  // }
 
-    // // Get the visits list
-    // const response = await fetch(
-    //     visitsEndpoint,
-    //     { credentials: 'include' }
-    //   );
+  // // Get the visits list
+  // const response = await fetch(
+  //     visitsEndpoint,
+  //     { credentials: 'include' }
+  //   );
 
-    // if(!response.ok) {
-    //   console.log("Lookout: something went wrong!");
-    //   console.log(response.status);
-    //   console.log(await response.json().errors);
+  // if(!response.ok) {
+  //   console.log("Lookout: something went wrong!");
+  //   console.log(response.status);
+  //   console.log(await response.json().errors);
 
-    //   // TODO redirect to an error page and not the home
-    //   return {
-    //     status: 302,
-    //     redirect: "/"
-    //   };
-    // }
+  //   // TODO redirect to an error page and not the home
+  //   return {
+  //     status: 302,
+  //     redirect: "/"
+  //   };
+  // }
 
-    // const visitsList = await response.json();
-    // if(visitsList.length === 0) {
-    //   console.log("warn: there are no appointments!");
+  // const visitsList = await response.json();
+  // if(visitsList.length === 0) {
+  //   console.log("warn: there are no appointments!");
 
-    //   // TODO redirect to an error page
-    // }
+  //   // TODO redirect to an error page
+  // }
 
-    // console.log(visitsList);
+  // console.log(visitsList);
 
-    const visitsList = [
-      {
-        date: '25/02/2022',
-        time: '18:00',
-        clinic: 'San Raffaele di Milano',
-        doctor: {
-          name: 'Francesco',
-          surname: 'Vénir',
-          qualification: 'Stomaterapista'
-        },
-        patient: {
-          name: 'Ornella',
-          surname: 'Calabrese',
-          ssn: 'VNRLCU92S27D962A'
-        },
-        caregiver: {
-          name: 'Douglas',
-          surname: 'Calabrese',
-          relation: 'Fratello'
-        }
+  const visitsList = [
+    {
+      date: "25/02/2022",
+      time: "18:00",
+      clinic: "San Raffaele di Milano",
+      doctor: {
+        name: "Francesco",
+        surname: "Vénir",
+        qualification: "Stomaterapista",
       },
-      {
-        date: '26/02/2022',
-        time: '17:00',
-        clinic: 'San Raffaele di Milano',
-        doctor: {
-          name: 'Giuseppe',
-          surname: 'Pezzella',
-          qualification: 'Stomaterapista'
-        },
-        patient: {
-          name: 'Franco',
-          surname: 'Beppe',
-          ssn: 'VNRLCU92S27D962A'
-        },
-        caregiver: {
-          name: 'Maria',
-          surname: 'Santa',
-          relation: 'Sorella'
-        }
-      }
-    ];
+      patient: {
+        name: "Ornella",
+        surname: "Calabrese",
+        ssn: "VNRLCU92S27D962A",
+      },
+      caregiver: {
+        name: "Douglas",
+        surname: "Calabrese",
+        relation: "Fratello",
+      },
+    },
+    {
+      date: "26/02/2022",
+      time: "17:00",
+      clinic: "San Raffaele di Milano",
+      doctor: {
+        name: "Giuseppe",
+        surname: "Pezzella",
+        qualification: "Stomaterapista",
+      },
+      patient: {
+        name: "Franco",
+        surname: "Beppe",
+        ssn: "VNRLCU92S27D962A",
+      },
+      caregiver: {
+        name: "Maria",
+        surname: "Santa",
+        relation: "Sorella",
+      },
+    },
+  ];
 
-    return {
-      props: {
-        visits: visitsList
-        // 'userId': session.id,
-      }
-    };
-  }
-</script>
-
-<script>
-  import DetailedTile from '$lib/shared/DetailedTile.svelte';
-  import { goto } from '$app/navigation';
-  import StyledButton from '$lib/shared/StyledButton.svelte';
-
-  export let visits;
-  // export let userId;
-
-  function callStart(visit) {
-    // TODO
-  }
-
+  return {
+    props: {
+      visits: visitsList,
+      // 'userId': session.id,
+    },
+  };
+}
+function callStart(visit) {
   // TODO
+}
 </script>
 
-<svelte:head>
-  <title>I tuoi appuntamenti - OpenMed</title>
-</svelte:head>
-
-<div class="mx-4 my-4">
-  <h1 class="font-bold my-2">Scegli il tuo appuntamento</h1>
-  <p class="font-normal mb-8">Le visite programmate sono qui.</p>
-  {#each visits as v}
-    <DetailedTile title="Visita" subtitle={`${v.date} h ${v.time}`}>
+<template>
+  <div class="mx-4 my-4">
+    <h1 class="font-bold my-2">Scegli il tuo appuntamento</h1>
+    <p class="font-normal mb-8">Le visite programmate sono qui.</p>
+    <DetailedTile
+      v-for="v in visits"
+      title="Visita"
+      :subtitle="`${v.date} h ${v.time}`"
+    >
       <div slot="content">
         <div class="py-3">
           <h3>Appuntamento</h3>
-          <p>il {v.date}</p>
-          <p>h {v.time}</p>
+          <p>il {{ v.date }}</p>
+          <p>h {{ v.time }}</p>
         </div>
         <div class="py-3">
           <h3>Ambulatorio</h3>
-          <p>{v.clinic}</p>
+          <p>{{ v.clinic }}</p>
         </div>
         <div class="py-3">
           <h3>Medico</h3>
-          <p>{v.doctor.name} {v.doctor.surname}</p>
-          <p class="font-light text-brandText-400">{v.doctor.qualification}</p>
+          <p>{{ v.doctor.name }} {{ v.doctor.surname }}</p>
+          <p class="font-light text-brandText-400">
+            {{ v.doctor.qualification }}
+          </p>
         </div>
         <div class="py-3">
           <h3>Paziente</h3>
-          <p>{v.patient.name} {v.patient.surname}</p>
-          <p class="font-light">{v.patient.ssn}</p>
+          <p>{{ v.patient.name }} {{ v.patient.surname }}</p>
+          <p class="font-light">{{ v.patient.ssn }}</p>
         </div>
         <div class="py-3">
           <h3>Caregiver / Familiare</h3>
-          <p>{v.caregiver.name} {v.caregiver.surname}</p>
-          <p class="font-light text-brandText-400">{v.caregiver.relation}</p>
+          <p>{{ v.caregiver.name }} {{ v.caregiver.surname }}</p>
+          <p class="font-light text-brandText-400">
+            {{ v.caregiver.relation }}
+          </p>
         </div>
       </div>
       <div
-        on:click|stopPropagation={() => null}
+        @click.stop="() => null"
         slot="trailing"
         class="flex flex-col items-stretch cursor-default"
       >
-        <StyledButton on:click={() => goto(`/call?id=${v.id}`)}>
-          <span class="material-icons-round align-middle pr-2">call</span> Inizio chiamata
+        <StyledButton @click="() => navigateTo(`/call?id=${v.id}`)">
+          <span class="material-icons-round align-middle pr-2">call</span>
+          Inizio chiamata
         </StyledButton>
         <StyledButton
           colors="bg-brandBlue-50/20 hover:bg-brandBlue-50/40 text-center"
-          on:click={() => goto(`/appointments/edit?id=${v.id}`)}
+          @click="() => navigateTo(`/appointments/edit?id=${v.id}`)"
         >
           Modifica
         </StyledButton>
       </div>
     </DetailedTile>
-  {:else}
-    <p>Non hai appuntamenti da poter mostrare qui.</p>
-  {/each}
-</div>
+    <p v-if="!visits">Non hai appuntamenti da poter mostrare qui.</p>
+  </div>
+</template>
--- client/pages/appointments/ok.vue
+++ client/pages/appointments/ok.vue
@@ -1,59 +1,65 @@
-<svelte:head>
-  <title>Tutto fatto! - OpenMed</title>
-</svelte:head>
+<script setup>
+useHead({
+  title: `Tutto fatto!`,
+});
+</script>
+<template>
+  <div
+    class="flex flex-col justify-center items-center rounded-3xl bg-brandBlue-50/40 mx-4 p-10 my-36"
+  >
+    <h1 class="font-bold text-center">Appuntamento registrato correttamente</h1>
+    <p class="my-2 text-center">Continua la navigazione</p>
+    <div class="my-7" />
+    <NuxtLink
+      to="/"
+      class="bg-brandBlue-500 text-white text-center px-3 py-2 rounded-3xl font-bold"
+    >
+      Torna in Home
+    </NuxtLink>
+    <!-- <div on:click={() => goto('/appointments/edit')}>Crea Nuovo Appuntamento</div> -->
 
-<div
-  class="flex flex-col justify-center items-center rounded-3xl bg-brandBlue-50/40 mx-4 p-10 my-36"
->
-  <h1 class="font-bold text-center">Appuntamento registrato correttamente</h1>
-  <p class="my-2 text-center">Continua la navigazione</p>
-  <div class="my-7" />
-  <a href="/" class="bg-brandBlue-500 text-white text-center px-3 py-2 rounded-3xl font-bold">
-    Torna in Home
-  </a>
-  <!-- <div on:click={() => goto('/appointments/edit')}>Crea Nuovo Appuntamento</div> -->
-
-  <!-- Aggiungere il + in fondo a dx -->
-</div>
-<div class="fixed -bottom-20 right-0 mx-4">
-  <div class="my-3 group flex flex-row justify-end items-center">
-    <p
-      class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
-    >
-      Altro appuntamento
-    </p>
-    <a
-      href="/appointments/edit"
-      class="p-2 text-4xl material-icons-round rounded-full bg-brandBlue-500 font-bold text-white"
-    >
-      add
-    </a>
+    <!-- Aggiungere il + in fondo a dx -->
   </div>
-  <div class="my-3 group flex flex-row justify-end items-center">
-    <p
-      class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
-    >
-      Torna indietro e modifica
-    </p>
-    <a
-      href="/appointments/edit"
-      class="p-2 text-4xl material-icons-round rounded-full bg-brandBlue-500 font-bold text-white"
-    >
-      edit
-    </a>
+  <div class="fixed -bottom-20 right-0 mx-4">
+    <div class="my-3 group flex flex-row justify-end items-center">
+      <p
+        class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
+      >
+        Altro appuntamento
+      </p>
+      <NuxtLink
+        to="/appointments/edit"
+        class="p-2 text-4xl material-icons-round rounded-full bg-brandBlue-500 font-bold text-white"
+      >
+        add
+      </NuxtLink>
+    </div>
+    <div class="my-3 group flex flex-row justify-end items-center">
+      <p
+        class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
+      >
+        Torna indietro e modifica
+      </p>
+      <NuxtLink
+        to="/appointments/edit"
+        class="p-2 text-4xl material-icons-round rounded-full bg-brandBlue-500 font-bold text-white"
+      >
+        edit
+      </NuxtLink>
+    </div>
+    <div class="my-3 group flex flex-row justify-end items-center">
+      <p
+        class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
+      >
+        Annulla appuntamento
+      </p>
+      <!-- TODO change this so that an appointments get deleted after a confirmation message -->
+      <NuxtLink
+        to=""
+        class="p-2 text-4xl material-icons-round rounded-full bg-brandBlue-500 font-bold text-white"
+      >
+        delete_forever
+      </NuxtLink>
+    </div>
   </div>
-  <div class="my-3 group flex flex-row justify-end items-center">
-    <p
-      class="font-bold group-hover:inline-block hidden px-3 mx-2 py-2 text-right bg-brandBlue-50/50 rounded-xl"
-    >
-      Annulla appuntamento
-    </p>
-    <!-- TODO change this so that an appointments get deleted after a confirmation message -->
-    <a
-      href=""
-      class="p-2 text-4xl material-icons-round rounded-full bg-brandBlue-500 font-bold text-white"
-    >
-      delete_forever
-    </a>
-  </div>
-</div>
+</template>
