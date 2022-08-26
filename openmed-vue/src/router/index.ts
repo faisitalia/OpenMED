91bd0c6c013709a8bb371a8229daf5fa3bd4e695
venir.dev <luca@venir.dev>
2022-08-26 10:50:28 +0200

    Bugfix

--- openmed-vue/src/router/index.ts
+++ openmed-vue/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
+import HomeView from "../pages/HomeView.vue";
+import AboutView from "../pages/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
