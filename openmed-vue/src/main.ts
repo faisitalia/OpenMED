3007adeda3c8e2cb004263212b69432159da24dc
venir.dev <luca@venir.dev>
2022-08-29 17:20:01 +0200

    re-installed Tailwind

--- openmed-vue/src/main.ts
+++ openmed-vue/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
 
+import "./index.css";
 import App from "./App.vue";
 import router from "./router";
 
