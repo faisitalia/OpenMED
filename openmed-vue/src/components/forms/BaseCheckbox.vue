bf7b94310497d08bc94c6d5ae2f427210c9abcc6
venir.dev <luca@venir.dev>
2022-08-30 14:38:17 +0200

    Created a few basic input form components (as a trial)

---
+++ openmed-vue/src/components/forms/BaseCheckbox.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// TODO the following may not work
+// TODO two way data binding with checkboxes is different
+import { computed } from "vue";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emitters = defineEmits(["update:value"]);
+
+const v = computed({
+  get: () => props.value,
+  set: (val) => emitters("update:value", val),
+});
+</script>
+
+<template>
+  <input type="checkbox" :id="label" v-model="v" />
+  <label v-if="label" :for="label">{{ label }}</label>
+</template>
---
+++ openmed-vue/src/components/forms/BaseInput.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// TODO improved a11y with:
+//    - uuids instead of using label as id
+//    - use aria-describedby to bind label + inputs + errors together
+//    - use aria-live="assertive" to read errors when present
+//    - use aria-invalid to show when errors are present
+
+import { computed } from "vue";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: [String, Number],
+    default: "",
+  },
+});
+
+const emitters = defineEmits(["update:value"]);
+
+const v = computed({
+  get: () => props.value,
+  set: (val) => emitters("update:value", val),
+});
+</script>
+
+<template>
+  <label :for="label">{{ label }}</label>
+  <input v-bind="$attrs" :id="label" :placeholder="label" v-model="v" />
+</template>
