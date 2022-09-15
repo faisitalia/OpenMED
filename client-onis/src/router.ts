d50f644153859bbfdd9c843fcfbe0dc38ddc2ccf
venir <luca@venir.dev>
2022-09-15 10:15:15 +0200

    Cleanup

--- client-onis/src/router.ts
+++ client-onis/src/router.ts
@@ -5,7 +5,6 @@ import { useUser } from "./composables/useUser";
 
 import HomePage from "@/pages/home/HomePage.vue";
 import LoginPage from "@/pages/login/LoginPage.vue";
-import UsersPageVue from "@/pages/users/UsersPage.vue";
 import SurveysPage from "./pages/surveys/SurveysPage.vue";
 
 const router = createRouter({
@@ -23,12 +22,6 @@ const router = createRouter({
       component: LoginPage,
     },
     {
-      path: "/users",
-      name: "users",
-      component: UsersPageVue,
-      meta: { layout: "OpenmedLayout" },
-    },
-    {
       path: "/surveys",
       name: "surveys",
       component: SurveysPage,
@@ -44,24 +37,6 @@ router.beforeEach(async (to, from) => {
   const { isAuthenticated } = useAuth();
 
   if (authenticationRequired && !isAuthenticated) return "/login";
-
-  const { isDoctor, isPatient, isAdmin } = useUser();
-
-  const adminPages = ["/users"];
-  const adminRequired = adminPages.includes(to.path);
-  if (adminRequired && !isAdmin) return from.path;
-
-  const patientPages = ["/appointments"];
-  const patientRequired = patientPages.includes(to.path);
-  if (patientRequired && !isPatient) return from.path;
-
-  const doctorPages = [
-    "/appointments",
-    "/appointments/edit",
-    "/appointments/ok",
-  ];
-  const doctorRequired = doctorPages.includes(to.path);
-  if (doctorRequired && !isDoctor) return from.path;
 });
 
 export default router;
