25e787566703dc3526b00a921148fb92adae0803
venir <luca@venir.dev>
2022-09-27 13:02:20 +0200

    Added some stylings to the surveys

--- client-onis/src/pages/surveys/SurveysPage.vue
+++ client-onis/src/pages/surveys/SurveysPage.vue
@@ -18,27 +18,69 @@ useHead({
   ],
 });
 
-let surveyScript;
 onMounted(() => {
   /* eslint-disable no-undef */
   Survey.StylesManager.applyTheme("defaultV2");
 
-  // console.log(surveyJson);
+  const styles = {
+    root: "bg-primary-light rounded-xl px-8 py-4 max-w-2xl",
+    headerText: "p-4 text-center",
+    header: "flex flex-col justify-center items-center",
+    title: "text-primary-dark self-center text-3xl",
+    description: "p-4 text-center text-sm italic",
+    question: {
+      number: "inline pr-2",
+      mainRoot: "py-4",
+      title: "text-xl",
+      requiredText: "text-primary-dark",
+      content: "text-md",
+    },
+    error: {
+      root: "text-sm",
+      tooltip: "text-primary-dark",
+      aboveQuestion: "text-primary-dark",
+      item: "text-primary-dark font-bold",
+    },
+    text: { small: "text-primary-dark font-bold" },
+    boolean: { mainRoot: "flex justify-center items-center" },
+    checkbox: {
+      controlLabel: "flex items-center justify-center font-light mx-4 my-0",
+      // materialDecorator:
+      //   "w-5 h-5 bg-white rounded-full checked:w-16 hover:bg-primary-light border-2 border-primary-dark",
+    },
+    radiogroup: {
+      controlLabel: "flex items-center justify-center font-light mx-4 my-0",
+      item: "flex items-center p-2 my-2",
+      label: "flex items-center",
+      itemChecked: "bg-primary rounded-md",
+      materialDecorator:
+        "peer-checked:bg-primary-dark w-5 h-5 bg-white rounded-full hover:bg-primary-light border-2 border-primary-dark",
+    },
+  };
 
   const survey = new Survey.Model(surveyJson);
   function alertResults(sender) {
     const results = JSON.stringify(sender.data);
     alert(results); // TODO send POST REQUEST
   }
+  survey.locale = "it";
   survey.onComplete.add(alertResults);
 
   $(function () {
-    $("#survey").Survey({ model: survey });
+    $("#survey").Survey({ model: survey, css: styles });
   });
 });
 </script>
 
 <template>
-  <h1 class="p-12">This is your survey:</h1>
-  <div id="survey"></div>
+  <div id="survey" class=""></div>
 </template>
+
+<style scoped>
+body #survey {
+  --primary: #e25831;
+  --background-dim-light: #e25831;
+  --foreground: #ffcfbc;
+  --foreground-light: #ffcfbc;
+}
+</style>
